438b3b5b4510310a33ac58df0ee35bdff35da043
Daniels Holden <danielsholden.my@gmail.com>
2023-07-15 18:55:38 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import img from './kid.png';
+import arrow from './arrows.png';
 import './App.css';
 
+import { languages, descriptions, guests } from './helper';
+
 function App() {
+  const searchParams = new URLSearchParams(window.location.search);
+  const langParam = searchParams.get('lang');
+  const lang = languages.includes(langParam) ? langParam : 'en';
+  const guest = searchParams.get('guest');
+
+  if (!guests[guest]) {
+    return null;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Dear, {guests[guest]}</h1>
+        <div className="img-block">
+          <img src={img} className="image" alt="logo" />
+          <div className='name'>INNA PIPIA</div>
+          <div className='arrow-block'>
+            <img src={arrow} className='arrow' alt='arrow' />
+            <div className="remember">Remember<br/>this kid?</div>
+          </div>
+        </div>
+        <div className="invitation">
+          {descriptions[lang]}
+          <br/>
+          <a href='https://goo.gl/maps/PfciL6nd4GNuqrK1A' target='_blank'>MAPS</a>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default React.memo(App);
---
+++ src/helper.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export const languages = ['en', 'ge', 'ru'];
+
+export const guests = {
+  sivtsovi: 'Бая и Саша',
+  mama: 'Мама',
+  eka: 'ეკა',
+  uta: 'Uta',
+  'luka-and-inga': 'ლუკა და ინგა დეიდა',
+  'mediko-and-tako': 'მედიკო დეიდა და თაკო',
+  'bero-vero-vache': 'ბერო, ვერო ფა ვაჩე',
+  sally: 'Sally',
+  'niko-and-keti': 'Niko and Keti',
+  dima: 'Дима',
+  sasha: 'Саша',
+  iliya: 'Илья',
+  nechitailo: 'Гриша и Эва',
+  rassohini: 'Стас, Женя и Лера',
+  alia: 'Алия',
+  'izoria': 'Levani, Olya and Mari',
+  mamaS: 'Ирина Геннадьевна',
+  alexS: 'Леша',
+  samarcevi: 'Алена и Костя'
+};
+
+export const descriptions = {
+  en: (
+    <>
+      is turning 30
+      <br/>
+      join her in toasting
+      <br/>
+      this grand achievement 
+      <br/>
+      <b>July 31st</b>
+      <br/>
+      <b>At 18-30</b>
+      <br/>
+      Kus Tba Street
+      <br/>
+      <b>Ciskari restaurant </b>
+      <br/>
+      Tbilisi
+    </>
+  ),
+  ru: (
+    <>
+      исполняется 30 лет
+      <br/>
+      Отпразднуйте вместе с ней
+      <br/>
+      Это важное событие 
+      <br/>
+      <b>31 Июля</b>
+      <br/>
+      <b>В 18.30</b>
+      <br/>
+      Черепашье озеро 
+      <br/>
+      <b>Ресторан Ciskari </b>
+      <br/>
+      Тбилиси
+    </>
+  ),
+  ge: (
+    <>
+      ხდება 30
+      <br/>
+      იზეიმეთ მასთან ერთად
+      <br/>
+      ეს მნიშვნელოვანი მოვლენაა
+      <br/>
+      <b>31 ივლისი</b>
+      <br/>
+      <b>18:30 საათზე</b>
+      <br/>
+      კუს ტბა
+      <br/>
+      <b>რესტორანი ცისკარი</b>
+      <br/>
+      თბილისი
+    </>
+  )
+}
